0021be823dfd240ac497b773015ec6c7429374be
mootil <mootility@gmail.com>
2021-11-22 14:58:53 +0900

    OSS.cpp 최종완성본
    메인 파일은 OSS.cpp, 헤더 파일은 원래 있던 HWClass.h와 Calendar.h 두 개를 쓰시면 됩니다.

---
+++ Calendar.h
@@ -0,0 +1,173 @@
+#pragma once
+#include "HWClass.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
+#include <string>
+
+static int isLeap = 0;
+static int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+// ------------------ 캘린더 함수 -----------------//
+int dayNumber(int day, int month, int year)
+{
+	static int t[] = { 0, 3, 2, 5, 0, 3, 5, 1,
+					4, 6, 2, 4 };
+	year -= month < 3;
+	return (year + year / 4 - year / 100 +
+		year / 400 + t[month - 1] + day) % 7;
+}
+
+int numberOfDays(int monthNumber, int year)
+{
+	// Janunary
+	if (monthNumber == 0)
+		return (31);
+
+	// February 
+	if (monthNumber == 1)
+	{
+		// If the year is leap then February has 
+		// 29 days 
+		if (year % 400 == 0 ||
+			(year % 4 == 0 && year % 100 != 0)) {
+			isLeap = 1;
+			return (29);
+		}
+		else {
+			isLeap = 0;
+			return (28);
+		}
+	}
+
+
+	// March 
+	if (monthNumber == 2)
+		return (31);
+
+	// April 
+	if (monthNumber == 3)
+		return (30);
+
+	// May 
+	if (monthNumber == 4)
+		return (31);
+
+	// June 
+	if (monthNumber == 5)
+		return (30);
+
+	// July 
+	if (monthNumber == 6)
+		return (31);
+
+	// August 
+	if (monthNumber == 7)
+		return (31);
+
+	// September 
+	if (monthNumber == 8)
+		return (30);
+
+	// October 
+	if (monthNumber == 9)
+		return (31);
+
+	// November 
+	if (monthNumber == 10)
+		return (30);
+
+	// December 
+	if (monthNumber == 11)
+		return (31);
+}
+
+// 어차피 난이도는 여기에 표시해 줄 필요가 없어서 지워버림.(어차피 입력값에서 그대로 받아오니까)
+int calculateCalendar(int month, int day) {
+	//1월부터 12월
+	int result = 0;
+
+	for (int i = 0; i < month-1; i++) {
+		result += days[i];
+	}
+
+	result += day;
+
+	//윤년일 때는 하루 더해줌
+	if (isLeap && month > 2) {
+		result += 1;
+	}
+
+	return result;
+}
+// 뒤 배열은 어차피 main쪽이랑 공유됨. 배열 자체 리턴이 안되서 이런식으로 바꿈.
+void recalculateCalendar(int date, int arr[]) { 
+
+	int input = date;
+	int month = 1;
+	int day = 0;
+
+	for (int i = 0; i < 12; i++) {
+		if (input - days[i] > 0) {
+			month++;
+			input -= days[i];
+		}
+		else {
+			day = input;
+		}
+	}
+
+	arr[0] = month;
+	arr[1] = day;
+}
+
+
+// Function to print the calendar of the given year 
+void printCalendar(int year, int arr[])
+{
+	cout << "\n		         Calendar - " << year << "\n";
+
+	int cdays; // 배열 days와 구분 위한 명칭 변경 days => cdays
+	int current = dayNumber(1, 1, year);
+
+	for (int i = 0; i < 12; i++)
+	{
+		cdays = numberOfDays(i, year);
+		cout << "\n ---------------------------- " << i + 1 << "월 -----------------------------\n\n";
+		cout << "	Sun	Mon	Tue	Wed	Thu	Fri	Sat\n";
+
+		int k;
+		for (k = 0; k < current; k++)
+			cout << "\t";
+
+		for (int j = 1; j <= cdays; j++) {
+			// 일치 여부 확인용 boolean 변수
+			bool cor = false;
+			// i=월, j=일. i,j를 받아가서 숫자로 변환하고 그 숫자랑 가져온 배열에 들어있는 값 중 하나라도 일치한다면 뒤에 *표시 넣어주면 됨
+			for (int m = 0; m < 10; m++) {
+				if (calculateCalendar(i+1, j) == arr[m])
+					cor = true;
+			}
+
+			// 일치시 *표시
+			if (cor) 
+				cout << '\t' << j << '*';
+			else
+				cout << '\t' << j;
+
+			if (++k > 6)
+			{
+				k = 0;
+				cout << "\n";
+			}
+		}
+
+		if (k)
+			cout << "\n";
+
+		current = k;
+	}
+	return;
+}
+// ------------------ 캘린더 함수 -----------------//
---
+++ OSS.cpp
@@ -0,0 +1,128 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "HWClass.h"
+#include "Calendar.h"
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int inputsize = 0;
+
+	ofstream file_obj;
+	file_obj.open("Input.txt", ios::app | ios::binary);
+
+	while (1) {
+		string input;
+
+		cout << "캘린더를 보시려면 q, 과제를 입력하시려면 해당 과제의 월을 입력해주세요 : ";
+		cin >> input;
+
+		if (input.compare("q") == 0) {
+			cout << ">> 캘린더를 확인합니다." << endl;
+			break;
+		}
+		int month = stoi(input);
+
+		cout << "<< 일을 입력하세요 : ";
+		int date;
+		cin >> date;
+
+		string name = "";
+		cout << "<< 과제명을 입력하세요(띄어쓰기 없이 입력하세요) : ";
+		cin >> name;
+
+		int difficulty = 1;
+		cout << "<< 난이도를 입력하세요 : ";
+		cin >> difficulty;
+
+		HW homework(date, month, name, difficulty);
+
+		file_obj.write((char*)&homework, sizeof(homework));
+
+		cout << ">> 성공적으로 추가되었습니다. " << endl;
+		inputsize++;
+	}
+	file_obj.close();
+
+	cout << ">> " << inputsize
+		<< " students info was successfully saved to the " << "input.txt" << endl;
+
+	ifstream ifile_obj;
+	ifile_obj.open("Input.txt", ios::in | ios::binary);
+	HW readhomework;
+
+	//입력받은 HW배열이라고 가정 10개로 일단 고정..
+	HW arr[10];
+
+	//과제 갯수가 총 몇 개인줄 세주는 변수
+	int num = 0;
+
+	//input에 있는 달력 정보 읽어오는 기능
+	do {
+		ifile_obj.read((char*)&readhomework, sizeof(readhomework));
+		arr[num] = readhomework;
+		num++;
+		ifile_obj.peek();
+	} while (!ifile_obj.eof());
+
+	ifile_obj.close();
+	
+	//얘는 365로 바꾼 배열
+	int arr365[10];
+
+	//다시 월/일/난이도로 돌린 HW 타입 배열 -> 일단 길이 10이라고 가정
+	HW arr2[10];
+
+	for (int i = 0; i < num; i++) {
+		arr365[i] = calculateCalendar(arr[i].returnmonth(), arr[i].returndate()); // arr365에 정보 입력
+
+		int dm[2]; // recalculateCalendar에서 월/일 공유하는 배열
+
+		recalculateCalendar(arr365[i], dm); // arr365 -> arr2로 다시 되돌려주는 과정
+		arr2[i].setHWname(arr[i].returnHWname());
+		arr2[i].setmonth(dm[0]);
+		arr2[i].setdate(dm[1]);
+		arr2[i].setDifficulty(arr[i].returndifficulty());
+	}
+
+	printCalendar(2021, arr365);
+	
+	while (1) {
+		int select;
+		cout << "1번 : 요일 별 과제 확인 / 2번 : 종료\n";
+		cout << "원하는 작업을 입력하세요 : ";
+		cin >> select;
+
+		if (select == 1) {
+			int month, date, exist;
+			cout << "월을 입력하세요.\n";
+			cin >> month;
+			cout << "일을 입력하세요.\n";
+			cin >> date;
+			cout << '\n';
+
+			bool isExist = false;
+			for (int m = 0; m < 10; m++) {
+				if (calculateCalendar(month, date) == arr365[m]) {
+					isExist = true;
+					exist = m;
+				}
+			}
+
+			if (isExist) {
+				cout << "|--------------------------|\n";
+				cout << "과제 이름 : " << arr2[exist].returnHWname() << '\n';
+				cout << arr2[exist].returnmonth() << " 월 " << arr2[exist].returndate() << " 일\n";
+				cout << "난이도 : " << arr2[exist].returndifficulty() << '\n';
+				cout << "|--------------------------|\n\n";
+			}
+			else
+				cout << "해당 날짜에 저장된 과제가 없습니다." << '\n';
+		}
+		else if (select == 2)
+			break;
+		else
+			cout << "잘못된 번호를 입력하셨습니다. 다시 입력하세요\n" << '\n';
+	}
+	return 0;
+}
